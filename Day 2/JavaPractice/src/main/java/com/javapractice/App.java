99aa977ff7f7e4c935f3c09206b8a2be954ae751
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-11 15:54:32 +0530

    making changes to the branch

--- Day 2/JavaPractice/src/main/java/com/javapractice/App.java
+++ Day 2/JavaPractice/src/main/java/com/javapractice/App.java
@@ -9,8 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+//        these are the two lines which whose conflict is resolved 
         System.out.println( "Hello World! From Local Repository" );
         System.out.println( "Hello World! from Remote Repository " );
-
+        hello();
+    }
+    
+    public static void hello()
+    {
+    	System.out.println("Hello Every One this is done in the new Branch ");
     }
 }
